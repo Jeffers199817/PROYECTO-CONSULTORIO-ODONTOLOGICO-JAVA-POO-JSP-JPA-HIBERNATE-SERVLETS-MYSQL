ec142bf01370f945440c06302e645b9ba1fec1fc
Jefferson Alquinga <144504631+Jeffers199817@users.noreply.github.com>
2024-11-05 22:23:21 -0500

    Integración de la clase paciente

--- src/main/java/logica/Odontologo.java
+++ src/main/java/logica/Odontologo.java
@@ -4,10 +4,29 @@
  */
 package logica;
 
+import java.util.Date;
+
 /**
  *
  * @author JEFFERSON ALQUINGA
  */
 public class Odontologo extends Persona{
+    
+    private int id_odontologo;
+    private String especialidad;
+
+    public Odontologo() {
+    }
+    
+    
+
+
+
+    public Odontologo(int id_odontologo, String especialidad, int id_persona, String dni, String nombre, String apellido, String telefono, String direccion, Date fecha_nac) {
+        super(id_persona, dni, nombre, apellido, telefono, direccion, fecha_nac);
+        this.id_odontologo = id_odontologo;
+        this.especialidad = especialidad;
+     
+    }
     
 }
--- src/main/java/logica/Paciente.java
+++ src/main/java/logica/Paciente.java
@@ -20,11 +20,35 @@ public class Paciente extends Persona{
         
     }
     
-    public Paciente(int id_paciente, boolean tiene_OS, String tipoSangre, int id_persona, String dni, String nombre, String apellido, String telefono, String direccion, Date fecha_nac ){
+    public Paciente(int id_paciente, boolean tiene_OS, String tipoSangre,  String dni, String nombre, String apellido, String telefono, String direccion, Date fecha_nac ){
         
-        super(id_persona,dni,nombre,apellido,telefono,direccion,fecha_nac);
+        super(dni,nombre,apellido,telefono,direccion,fecha_nac);
         this.id_paciente = id_paciente;
         this.tiene_OS = tiene_OS;
+        this.tipoSangre = tipoSangre;
+    }
+
+    public int getId_paciente() {
+        return id_paciente;
+    }
+
+    public void setId_paciente(int id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+
+    public boolean isTiene_OS() {
+        return tiene_OS;
+    }
+
+    public void setTiene_OS(boolean tiene_OS) {
+        this.tiene_OS = tiene_OS;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public void setTipoSangre(String tipoSangre) {
         this.tipoSangre = tipoSangre;
     }
     
